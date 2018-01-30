7c95ee877ea62047e123bd79b4a805b6373b2d76
Admin <anjarusnak19@gmail.com>
2018-01-30 23:08:43 +0200

    Homework#1. Gmail test

---
+++ seleniumpractice/src/test/java/com/epam/lab/GoogleTest.java
@@ -0,0 +1,72 @@
+package com.epam.lab;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class GoogleTest {
+    private WebDriver webDriver;
+
+    @Test
+    public void gmailTest() {
+        System.setProperty( "webdriver.chrome.driver", "src/resourses/chromedriver.exe" );
+        webDriver = new ChromeDriver();
+        webDriver.get( "https://accounts.google.com/signin/v2/identifier?continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&osid=1&service=mail&ss=1&ltmpl=default&rm=false&flowName=GlifWebSignIn&flowEntry=ServiceLogin" );
+        webDriver.manage().timeouts().implicitlyWait( 15, TimeUnit.SECONDS );
+        webDriver.manage().window().maximize();
+        WebElement enterEmail = webDriver.findElement( By.xpath( "//*[@id='identifierId']" ) );
+        enterEmail.sendKeys( "annrusnaktest12345@gmail.com" );
+        webDriver.findElement( By.xpath( "//*[@id='identifierNext']/content/span" ) ).click();
+        WebElement password = webDriver.findElement( By.xpath( ".//*[@id='password']//descendant::input" ) );
+        WebDriverWait wait = new WebDriverWait( webDriver, 20 );
+        password.sendKeys( "annatest123" );
+        wait.until( ExpectedConditions.elementToBeClickable( password ) );
+        WebElement nextButton = webDriver.findElement( By.xpath( ".//*[@id='passwordNext']//descendant::span" ) );
+        Actions builder = new Actions( webDriver );
+        builder.click( nextButton ).build().perform();
+        List<WebElement> checkbox = webDriver.findElements( By.xpath( ".//*[@role='checkbox']" ) );
+        //List<WebElement> subject = webDriver.findElements( By.xpath( "//*[@class='bog']" ) );
+        //  List<String > strings = new ArrayList<String>() ;
+        for (int i = 1; i < 4; i++) {
+            if (!checkbox.get( i ).isSelected()) {
+                checkbox.get( i ).click();
+                //    strings.set( i,subject.get( i-1 ).getText() );
+            }
+        }
+
+
+        WebElement delete = webDriver.findElement( By.xpath( ".//*[@id=':5']/div/div[1]/div[1]/div/div/div[2]/div[3]/div/div" ) );
+        delete.click();
+        WebElement undo = webDriver.findElement( By.xpath( ".//*[@id='link_undo']" ) );
+        wait.until( ExpectedConditions.elementToBeClickable( undo ) );
+        undo.click();
+        int actualResult = 0;
+        List<WebElement> sub = webDriver.findElements( By.xpath( "//*[@class='bog']" ) );
+        for (int i = 0; i < sub.size(); i++) {
+            if (sub.get( i ).getText().equals( "HomePod. The new sound of home." )) {
+                actualResult += 1;
+                break;
+            }
+            Assert.assertNotEquals(0, actualResult);
+        }
+    }
+
+    @AfterMethod
+    public final void tearDown() {
+        webDriver.quit();
+    }
+
+
+}
