1a6cb75f019dd94f06cc9041c8a5c3cfaa06de1b
bohdan <dovbysh.bohdan@gmail.com>
2018-01-30 17:21:49 +0200

    Complete task
    Complete task

--- src/test/java/LoginGmail.java
+++ src/test/java/LoginGmail.java
@@ -1,3 +1,4 @@
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -13,10 +14,10 @@ public class LoginGmail {
     private static WebDriver driver;
     private static String DRIVER_URL = "C:\\Users\\admin\\eclipse-workspace\\GmailTest\\src\\main\\resources\\chromedriver.exe";
     private static String URL = "https://accounts.google.com/signin";
-    private static String YOUR_EMAIL = "dovbysh.bohdan@gmail.com";
-    private static String PASSWORD = "FuckOff666666719113";
-    private static String SEND_TO = "dovbysh.bohdan@gmail.com";
-    private static String SUBJECT = "Test";
+    private static String YOUR_EMAIL = "homeworkbohdan@gmail.com";
+    private static String PASSWORD = "719113719113";
+    private static String SEND_TO = "homeworkbohdan@gmail.com";
+    private static String SUBJECT = "TestTwo";
     private static String MESSAGE = "Test message";
 
     @BeforeClass
@@ -38,33 +39,31 @@ public class LoginGmail {
         password.sendKeys(PASSWORD);
         driver.findElement(By.className("CwaK9")).click();
         driver.findElement(By.className("WaidBe")).click();
-    }
 
-    @Test
-    public void fillToMessage() {
         driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
         WebElement send = driver.findElement(By.xpath("//div[@class='T-I J-J5-Ji T-I-KE L3']"));
         driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
         send.click();
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-        driver.findElement(By.xpath("//textarea[@id=':mp']")).sendKeys(SEND_TO);
+        driver.findElement(By.xpath("//textarea[@class='vO']")).sendKeys(SEND_TO);
         driver.findElement(By.xpath("//input[@class='aoT']")).sendKeys(SUBJECT);
         driver.findElement(By.xpath("//div[@class='Am Al editable LW-avf']")).sendKeys(MESSAGE);
-    }
-    @Test
-    public void clickSendButton(){
+
         driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
-        WebElement sendButton =  driver.findElement(By.xpath("//div[@class='J-J5-Ji btA']"));
+        WebElement sendButton = driver.findElement(By.xpath("//div[@class='J-J5-Ji btA']"));
         driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
         sendButton.click();
+
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.findElement(By.linkText("Sent Mail")).click();
+
+        driver.manage().timeouts().implicitlyWait(25, TimeUnit.SECONDS);
+        String messageElement = driver.findElement(By.xpath("//*[@class='bog']//*[text()='TestTwo']")).getText();
+        Assert.assertEquals("TestTwo", messageElement);
+        
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.findElement(By.xpath("//td[@class='oZ-x3 xY']")).click();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.findElement(By.xpath("//div[@act='10']")).click();
     }
-//    @Test
-//    public void removeFromSend(){
-//        driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
-//        WebElement sendFolder = driver.findElement(By.xpath("//div[@class='aio UKr6le']"));
-//        sendFolder.click();
-//        driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
-//        WebElement myMessage = driver.findElement(By.linkText("мені"));
-//        myMessage.click();
-//    }
 }
